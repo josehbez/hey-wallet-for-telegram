b106e3078ad902876a6dab8bfd906aba60f8f878
José Hernández <jose.hbez@gmail.com>
2020-08-27 16:05:45 -0500

    add data demo

--- datasource/odoo.py
+++ datasource/odoo.py
@@ -2,6 +2,7 @@ import xmlrpc.client
 import json
 import re 
 from urllib.parse import urlparse
+import os 
 
 class Odoo:
 
@@ -20,19 +21,32 @@ class Odoo:
     def is_auth(self):
         return self.uid
 
+    def demo_creadentials(self):
+        if str(os.getenv('ENVIRONMENT')) == 'DEV':
+            return {
+                'url':os.getenv('ODOO_URL'),
+                'database':os.getenv('ODOO_DATABASE'),
+                'username':os.getenv('ODOO_USERNAME'),
+                'password':os.getenv('ODOO_PASSWORD'),
+            }
+        return None
+        
+        
+
     def auth(self, **args):
+        args = self.demo_creadentials() or args        
         up = urlparse(args.get(
             'url',
-            'http://167.99.171.117:6071/' #'https://wallet.maxs.biz'
+            'https://wallet.maxs.biz'
         ))
 
         url = '{}://{}'.format(
             up.scheme or 'https', up.netloc
         )
-        
-        self.username = 'admin@maxs.biz'#args.get('username')
-        self.password = 'h3yWall3t'#args.get('password')
-        self.database = 'heywallet.sandbox.maxs.biz' # args.get('database', 'wallet.maxs.biz')
+
+        self.username = args.get('username')
+        self.password = args.get('password')
+        self.database = args.get('database', 'wallet.maxs.biz')
         self.url = url
         self.uid = self.common().authenticate(self.database, self.username, self.password, {})
         return self.uid
